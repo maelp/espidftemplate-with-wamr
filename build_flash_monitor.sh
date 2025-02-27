8f1a7b815f4dc323245280da941af6a3d9673208
Mael Primet <mael.primet@gmail.com>
2025-02-27 17:11:43 +0100

    feat: this seems to work

--- build_flash_monitor.sh
+++ build_flash_monitor.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+. ~/export-esp.sh
+. ~/esp/esp-idf/export.sh
+export CARGO_FEATURE_ESP_IDF=1 
+export WAMR_BUILD_TARGET=XTENSA
+
 cargo espflash flash --monitor --port=/dev/cu.usbmodem1101 --release --target=xtensa-esp32s3-espidf --chip esp32s3
