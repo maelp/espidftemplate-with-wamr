afea1422ab535e09d4b83b9bc66bbc3c04fcc63c
Mael Primet <mael.primet@gmail.com>
2025-02-27 17:29:20 +0100

    feat: update code

--- build_flash_monitor.sh
+++ build_flash_monitor.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+set -eou pipefail
+
+if [ ! -e "~/export-esp.sh" ]; then
+    echo "~/export-esp.sh does not exist"
+    exit 1
+fi
+if [ ! -e "~/esp/esp-idf/export.sh" ]; then
+    echo "~/esp/esp-idf/export.sh does not exist"
+    exit 1
+fi
+
 . ~/export-esp.sh
 . ~/esp/esp-idf/export.sh
-export CARGO_FEATURE_ESP_IDF=1 
-export WAMR_BUILD_TARGET=XTENSA
 
-cargo espflash flash --monitor --port=/dev/cu.usbmodem1101 --release --target=xtensa-esp32s3-espidf --chip esp32s3
+cargo run --release
